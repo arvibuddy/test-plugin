6909b1247ec843bfb6a67d9520679538e1be2fe4
Arvind <arvind.yadav@tutanota.com>
2023-09-06 23:35:21 +0530

    Adding file1 and file2 js files

---
+++ file1.js
@@ -0,0 +1,12 @@
+import { greet } from './file2.js';
+
+
+function sayHello() {
+  console.log("Hello, World!");
+}
+
+// Call the function to display the message
+sayHello();
+
+// Call the imported function
+greet("Alice");
